12f2aa8d2e5787135413c8ee24ed2ea11cfd8a97
kou-art <kh603o1s@gmail.com>
2026-06-07 22:48:30 +0900

    add

--- ReservationAPI/Reservation/obj/Debug/net8.0/Reservation.AssemblyInfo.cs
+++ ReservationAPI/Reservation/obj/Debug/net8.0/Reservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57bb10e0bcd956b2c2b38d009815494e0b2e7582")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
